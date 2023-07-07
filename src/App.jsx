cf74b3c74ae9a73baaa0d5aa516d906626c171cd
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-08 00:43:51 +0100

    finished the mobile layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import StatsPreviewCard from './components/StatsPreviewCard'
+
+function App() {
+  return (
+    <div>
+      <StatsPreviewCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StatsPreviewCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import CardImg from '../assets/images/image-header-desktop.jpg'
+
+function StatsPreviewCard() {
+  return (
+    <div className='bg-[#1b1938] w-[340px] h-[820px] rounded-2xl'>
+        <div className='relative'>
+            <img className='rounded-t-2xl' src={CardImg} alt="card image" />
+            <div class="absolute inset-0 bg-overlay opacity-40 rounded-t-2xl"></div>  
+        </div>
+        
+
+        <div className='p-4 mt-4 text-white'>
+            <div>
+                <p className='line-height text-[2em] text-center font-bold'>
+                    Get <span className='  text-violet-500'>Insight</span> that help your bussiness grow.
+                </p>
+                <p className='leading-8 mt-6 opacity-60 text-center my-2 text-[20px]'>
+                    Discover the benefits of data analytics and make better dicision regarding revenue, customer experience and over all efficiency.
+                </p>
+            </div>
+            <div className='flex flex-col gap-4 justify-center items-center text-center'>
+                <div>
+                    <p className='font-bold text-[2rem]'>
+                        10k+
+                    </p>
+                    <p className='text-xs opacity-60'>
+                        COMPANIES
+                    </p>
+                </div>
+                <div>
+                    <p className='font-bold text-[2rem]'>
+                        314
+                    </p>
+                    <p className='text-xs opacity-60'>TEMPLATES</p>
+                </div>
+                <div>
+                    <p className='font-bold text-[2rem]'>12M+</p>
+                    <p className='text-xs opacity-60'>QUERIES</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default StatsPreviewCard
