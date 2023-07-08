095fd391ba08c92e1f98ecf4bf335c3df42ee13f
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-08 01:02:41 +0100

    finished the mobile layout

--- src/components/StatsPreviewCard.jsx
+++ src/components/StatsPreviewCard.jsx
@@ -3,7 +3,7 @@ import CardImg from '../assets/images/image-header-desktop.jpg'
 
 function StatsPreviewCard() {
   return (
-    <div className='bg-[#1b1938] w-[340px] h-[100vh] rounded-2xl'>
+<div className='bg-[#1b1938] w-[300px] h-[680px] rounded-2xl'>
         <div className='relative'>
             <img className='rounded-t-2xl' src={CardImg} alt="card image" />
             <div class="absolute inset-0 bg-overlay opacity-40 rounded-t-2xl"></div>  
@@ -15,7 +15,7 @@ function StatsPreviewCard() {
                 <p className='line-height text-[24px] text-center font-bold'>
                     Get <span className='  text-violet-500'>Insight</span> that help your bussiness grow.
                 </p>
-                <p className='leading-8 mt-2 opacity-60 text-center my-2 text-[16px]'>
+                <p className='leading-6 mt-2 opacity-60 text-center my-2 text-[12px]'>
                     Discover the benefits of data analytics and make better dicision regarding revenue, customer experience and over all efficiency.
                 </p>
             </div>
